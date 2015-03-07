a1de46a6255a50eeb13886bd9647580d36f6c86e
Fabrice Servais <fabrice.servais@gmail.com>
2015-03-07 11:18:00 +0100

    Cleaning script (still log to do)

--- Project 1/Script/test.py
+++ Project 1/Script/test.py
@@ -1,35 +1,36 @@
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
-cmdGen = cmdgen.CommandGenerator()
+def getNumberOfPackets(oid):
+    cmdGen = cmdgen.CommandGenerator()
 
-#oid = cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0), \
- #     cmdgen.MibVariable('SNMPv2-MIB', 'ipForwarding', 0), \
-  #    cmdgen.MibVariable('SNMPv2-MIB', 'ifNumber', 0)
+    errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+        cmdgen.CommunityData('run69Zork!'),
+        cmdgen.UdpTransportTarget(('hawk.run.montefiore.ulg.ac.be', 161)),
+        oid,
+        lookupNames=True, lookupValues=True
+    )
 
-oid = (1,3,6,1,2,1,1,1,0), \
-      (1,3,6,1,2,1,4,1,0), \
-      (1,3,6,1,2,1,2,1,0);
-#oid = sysDescr, ipForwarding, ifNumber
-
-errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-    cmdgen.CommunityData('run69Zork!'),
-    cmdgen.UdpTransportTarget(('hawk.run.montefiore.ulg.ac.be', 161)),
-    *oid,
-    lookupNames=True, lookupValues=True
-)
-
-# Check for errors and print out results
-if errorIndication:
-    print(errorIndication)
-else:
-    if errorStatus:
-        print('%s at %s' % (
-            errorStatus.prettyPrint(),
-            errorIndex and varBinds[int(errorIndex)-1] or '?'
-            )
-        )
+    if errorIndication:
+        print(errorIndication)
+        return
     else:
-        for name, val in varBinds:
-            print('%s = %s\n' % (name.prettyPrint(), val.prettyPrint()))
+        if errorStatus:
+            print('%s at %s' % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex)-1] or '?'
+                )
+            )
+            return
+        else:
+            for name, val in varBinds:
+                return val
+            return
+
+
+oid_received = (1,3,6,1,2,1,11,1,0)
+oid_sent = (1,3,6,1,2,1,11,2,0)
+
+packets_received = getNumberOfPackets(oid_received)
+packets_sent = getNumberOfPackets(oid_sent)
 
 
