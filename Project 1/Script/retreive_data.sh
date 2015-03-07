720cac19457aa308d1ebcb573d31949a091aa7a5
Fabrice Servais <fabrice.servais@gmail.com>
2015-03-07 12:59:19 +0100

    Final script (hope)

---
+++ Project 1/Script/retreive_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sshpass -p "we|Baa2oh" scp nms01@bee.run.montefiore.ulg.ac.be:NMS-script/matlab_data.dat matlab_data.dat
--- Project 1/Script/test.py
+++ Project 1/Script/test.py
@@ -1,3 +1,7 @@
+import time
+import logging
+import datetime
+
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 def getNumberOfPackets(oid):
@@ -26,11 +30,78 @@ def getNumberOfPackets(oid):
                 return val
             return
 
+#########################################
+logging.basicConfig(filename='data_packets.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
+# Objects to consult (snmpInPkts and snmpOutPkts)
 oid_received = (1,3,6,1,2,1,11,1,0)
 oid_sent = (1,3,6,1,2,1,11,2,0)
 
-packets_received = getNumberOfPackets(oid_received)
-packets_sent = getNumberOfPackets(oid_sent)
+# For the script to have a duration of 24h
+time_start = datetime.datetime.now()
+num_min = 0
+
+# Get first data
+start_number_received = getNumberOfPackets(oid_received)
+start_number_sent = getNumberOfPackets(oid_sent)
+
+prev_packets_received = start_number_received
+prev_packets_sent = start_number_sent
+
+diff_received = 0
+diff_sent = 0
+
+logging.info("| Number packets received (difference)    Number packets sent (difference)")
+logging.info("|              %d (+%d)                             %d (+%d)" % (prev_packets_received, diff_received, prev_packets_sent, diff_sent))
+
+# To generate data file for Matlab
+prev_matlab_received = prev_packets_received
+prev_matlab_sent = prev_packets_sent
+
+matlab_data = open('matlab_data.dat', 'a')
+message = "%d %d %d %d %d\n" % (num_min, prev_matlab_received, prev_matlab_sent, diff_received, diff_sent)
+matlab_data.write(message)
+while True:
+    # Wait 5 minutes 
+    time.sleep(300)
+    num_min += 5
+
+    packets_received = getNumberOfPackets(oid_received)
+    packets_sent = getNumberOfPackets(oid_sent)
+
+    # If the counter had wrapped
+    if(packets_received < prev_packets_received):
+        diff_received = (2**32 - 1 - prev_packets_received) + packets_received
+    else: 
+        diff_received = packets_received - prev_packets_received
+
+    if(packets_sent < prev_packets_sent):
+        diff_sent = (2**32 - 1 - prev_packets_sent) + packets_sent
+    else: 
+        diff_sent = packets_sent - prev_packets_sent
+
+    # Log
+    message = '|              %d (+%d)                             %d (+%d)' % (packets_received, diff_received, packets_sent, diff_sent)
+    logging.info(message)
+
+    prev_packets_received = packets_received
+    prev_packets_sent = packets_sent
+
+    # Matlab log
+    prev_matlab_received += diff_received
+    prev_matlab_sent += diff_sent
+    message = "%d %d %d %d %d\n" % (num_min, prev_matlab_received, prev_matlab_sent, diff_received, diff_sent)
+    matlab_data.write(message)
+
+    # Check duration of the executiin
+    time_now = datetime.datetime.now()
+
+    diff_time = time_now - time_start
+
+    if diff_time.days > 0:
+        break
+
+f.close()
 
 
+    
