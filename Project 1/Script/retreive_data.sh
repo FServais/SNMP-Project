9010ccb8b88a3945a155bd7fb17f11fc2c87bf33
Fabrice Servais <fabrice.servais@gmail.com>
2015-03-08 16:25:44 +0100

    Update get data script

--- Project 1/Script/retreive_data.sh
+++ Project 1/Script/retreive_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sshpass -p "we|Baa2oh" scp nms01@bee.run.montefiore.ulg.ac.be:NMS-script/matlab_data.dat matlab_data.dat
+sshpass -p "we|Baa2oh" scp nms01@bee.run.montefiore.ulg.ac.be:NMS-script/data_packets.log data_packets.log
+sshpass -p "we|Baa2oh" scp nms01@bee.run.montefiore.ulg.ac.be:NMS-script/nohup.out nohup.out
