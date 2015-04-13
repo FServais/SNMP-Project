aaa8d3fb90d3ce2655195ed5aab40ec6a0c872bc
Fabrice Servais <fabrice.servais@gmail.com>
2015-04-13 22:37:28 +0200

    clean

--- Project 2/Part2/snmptest.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-	$a = snmpwalk("hawk.run.montefiore.ulg.ac.be", "run69Zork!", "");
-
-	foreach ($a as $val) {
-	    echo "$val\n";
-	}
- ?>
