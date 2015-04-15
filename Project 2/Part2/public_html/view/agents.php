4f9525a36b09fbc2b480d45c90fbe241b1aa7266
Fabrice Servais <fabrice.servais@gmail.com>
2015-04-16 00:06:44 +0200

    Re design agents + clickable row

--- Project 2/Part2/public_html/view/agents.php
+++ Project 2/Part2/public_html/view/agents.php
@@ -4,24 +4,54 @@
   <meta charset="utf-8">
   <title>Agents present in the network</title>
   <link rel="stylesheet" href="view/style.css" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+  <script>
+	jQuery(document).ready(function($) {
+		$(".clickable-row").click(function() {
+			$("#agents .clickable-row tr td").toggleClass('clicked');
+		    window.document.location = $(this).data("href");
+		});
+	});
+  </script>
+
 </head>
 
 <body>
 	<h1> Agents present in the network </h1>
+		<div id="agents">
+			<table>
+				<tr>
+					<th>IP</th>
+					<th>Port</th>
+					<th>Version</th>
+					<th>Community name</th>
+				</tr>
+				
+				<?php 
+					foreach($agents_array as $key =>  $agent)
+					{
+						$ip = $agent["ip"];
+						$port = $agent["port"];
+						$version = $agent["version"];
+						$secname = $agent["secname"];
 
-	<?php 
-		foreach($agents_array as $key =>  $agent)
-		{
-			$ip = $agent["ip"];
-			$port = $agent["port"];
-			$version = $agent["version"];
-			$secname = $agent["secname"];
+						//echo '<a href="index.php?page=oid&ip=' . $ip . '&port=' . $port . '&version=' . $version . '&secname=' . $secname . '">' . $ip . ':' . $port . ' version: ' . $version . ' secname : ' . $secname . '  </a> </br>'; 
+						
 
-			echo '<a href="index.php?page=oid&ip=' . $ip . '&port=' . $port . '&version=' . $version . '&secname=' . $secname . '">' . $ip . ':' . $port . ' version: ' . $version . ' secname : ' . $secname . '  </a> </br>'; 
-			
-		}
+						echo "<tr class='clickable-row' data-href='index.php?page=oid&ip=" . $ip . "&port=" . $port . "&version=" . $version . "&secname=" . $secname . "'>";
 
-	?>
+							echo "<td>" . $ip . "</td>";
+							echo "<td>" . $port . "</td>";
+							echo "<td>" . $version . "</td>";
+							echo "<td>" . $secname . "</td>";
+
+						echo "</tr>";
+
+					}
+				?>
+			</table>
+		</div>
 
 </body>
 </html>
