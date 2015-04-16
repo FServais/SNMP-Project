d8da056e33373a4cadc710ec8b7b877a62e3d658
FlorianeM <floriane.magera@live.be>
2015-04-16 12:40:47 +0200

    View centralization + refresh

--- Project 2/Part2/public_html/view/agents.php
+++ Project 2/Part2/public_html/view/agents.php
@@ -1,57 +1,37 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>Agents present in the network</title>
-  <link rel="stylesheet" href="view/style.css" />
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
-  <script>
-	jQuery(document).ready(function($) {
-		$(".clickable-row").click(function() {
-			$("#agents .clickable-row tr td").toggleClass('clicked');
-		    window.document.location = $(this).data("href");
-		});
-	});
-  </script>
+<h1> Agents present in the network </h1>
+	<div id="agents">
+		<table>
+			<tr>
+				<th>IP</th>
+				<th>Port</th>
+				<th>Version</th>
+				<th>Community name</th>
+			</tr>
+			
+			<?php 
+				echo '<a href="index.php?page=agents&refresh=true"> Refresh agents </a> </br>'; 
+				foreach($agents_array as $key =>  $agent)
+				{
+					$ip = $agent["ip"];
+					$port = $agent["port"];
+					$version = $agent["version"];
+					$secname = $agent["secname"];
 
-</head>
+					
 
-<body>
-	<h1> Agents present in the network </h1>
-		<div id="agents">
-			<table>
-				<tr>
-					<th>IP</th>
-					<th>Port</th>
-					<th>Version</th>
-					<th>Community name</th>
-				</tr>
-				
-				<?php 
-					foreach($agents_array as $key =>  $agent)
-					{
-						$ip = $agent["ip"];
-						$port = $agent["port"];
-						$version = $agent["version"];
-						$secname = $agent["secname"];
+					echo "<tr class='clickable-row' data-href='index.php?page=oid&ip=" . $ip . "&port=" . $port . "&version=" . $version . "&secname=" . $secname . "'>";
 
-						//echo '<a href="index.php?page=oid&ip=' . $ip . '&port=' . $port . '&version=' . $version . '&secname=' . $secname . '">' . $ip . ':' . $port . ' version: ' . $version . ' secname : ' . $secname . '  </a> </br>'; 
-						
+						echo "<td>" . $ip . "</td>";
+						echo "<td>" . $port . "</td>";
+						echo "<td>" . $version . "</td>";
+						echo "<td>" . $secname . "</td>";
 
-						echo "<tr class='clickable-row' data-href='index.php?page=oid&ip=" . $ip . "&port=" . $port . "&version=" . $version . "&secname=" . $secname . "'>";
+					echo "</tr>";
 
-							echo "<td>" . $ip . "</td>";
-							echo "<td>" . $port . "</td>";
-							echo "<td>" . $version . "</td>";
-							echo "<td>" . $secname . "</td>";
+				}
+			?>
+		</table>
+	</div>
 
-						echo "</tr>";
 
-					}
-				?>
-			</table>
-		</div>
-
-</body>
-</html>
--- Project 2/Part2/public_html/view/index.php
+++ Project 2/Part2/public_html/view/index.php
@@ -4,7 +4,32 @@
   <meta charset="utf-8">
   <title>Agents present in the network</title>
   <link rel="stylesheet" href="view/style.css" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+  <script>
+	jQuery(document).ready(function($) {
+		$(".clickable-row").click(function() {
+			$("#agents .clickable-row tr td").toggleClass('clicked');
+		    window.document.location = $(this).data("href");
+		});
+	});
+  </script>
+
 </head>
+
+<body>
+
+	<?php
+
+		$pages = array('index', 'agents', 'oid', 'oidvalue');
+
+		/* Launch the corresponding controller (variable 'page' in URL)*/
+		if(isset($_GET['page']) && in_array($_GET['page'], $pages))
+			include_once('view/' . $_GET['page'] . '.php');
+		else
+			include_once('view/agents.php');
+
+	?>
 
 <body>
 	
